ddc08147ce927597e272660c1bcd1b3db6ffc754
JonathanPR24 <jonathanperez604@gmail.com>
2023-08-22 15:58:34 -0400

    Export to CSV

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """This script retrieves employee TODO list progress and exports it in CSV format"""
+import csv
 import requests
 import sys
-import csv
+
 
 if __name__ == "__main__":
     employee_id = int(sys.argv[1])
@@ -11,21 +12,35 @@ if __name__ == "__main__":
     response = requests.get(api_url)
 
     employee_name = response.json()["username"]
-    
-    api_url2 = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
+
+    api_url2 = (
+            f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
+    )
     response = requests.get(api_url2)
 
     tasks = response.json()
+    total_tasks = len(tasks)
 
-    csv_filename = f"{employee_id}.csv"
+    completed_tasks = []
+    for task in tasks:
+        if task["completed"]:
+            completed_tasks.append(task)
 
-    with open(csv_filename, mode="w", newline="") as csv_file:
-        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_NONNUMERIC)
-        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        
-        for task in tasks:
-            task_completed_status = "True" if task["completed"] else "False"
-            task_title = task["title"]
-            csv_writer.writerow([employee_id, employee_name, task_completed_status, task_title])
+    n_total_tasks = len(completed_tasks)
 
-    print(f"Data exported to {csv_filename}")
+    print(
+        f"Employee {employee_name} is done with "
+        f"tasks({n_total_tasks}/{total_tasks}):"
+    )
+
+    for task in completed_tasks:
+        print(f"\t {task['title']}")
+
+    rows = []
+    for task in tasks:
+        row = employee_id, employee_name, task["completed"], task["title"]
+        rows.append(row)
+
+    with open(f"{employee_id}.csv", 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+        writer.writerows(rows)
