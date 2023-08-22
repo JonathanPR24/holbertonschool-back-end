82ebe32938da532602f12b431ab12cdcc0f8d2e7
JonathanPR24 <jonathanperez604@gmail.com>
2023-08-22 15:39:26 -0400

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,39 +1,42 @@
 #!/usr/bin/python3
-"""Script to return information of todo list"""
+"""Script to return info about todo list progress"""
 import requests
-import sys
+from sys import argv
 
-def fetch_employee_todo_progress(employee_id):
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_url = f"{base_url}/users/{employee_id}"
-    todo_url = f"{base_url}/todos?userId={employee_id}"
+def information_employee():
+    """Returns information about employees"""
+    if len(argv) != 2:
+        print("Usage: python3 script_name.py employee_id")
+        return
+
+    id_employee = int(argv[1])
+
+    url_users = 'https://jsonplaceholder.typicode.com/users'
+    url_todos = 'https://jsonplaceholder.typicode.com/todos'
 
     try:
-        user_response = requests.get(user_url)
-        todo_response = requests.get(todo_url)
-
-        if user_response.status_code != 200 or todo_response.status_code != 200:
-            print("Error: Unable to fetch data from the API.")
-            return
-
-        user_data = user_response.json()
-        todo_data = todo_response.json()
-
-        employee_name = user_data["name"]
-        total_tasks = len(todo_data)
-        done_tasks = [task for task in todo_data if task["completed"]]
-        done_task_titles = [task["title"] for task in done_tasks]
-
-        print(f"Employee {employee_name} is done with tasks({len(done_tasks)}/{total_tasks}):")
-        for title in done_task_titles:
-            print(f"\t{title}")
-
+        response_users = requests.get(url_users)
+        response_todos = requests.get(url_todos)
+        response_users.raise_for_status()
+        response_todos.raise_for_status()
     except requests.exceptions.RequestException as e:
-        print("Error:", e)
+        print("Error fetching data from the API:", e)
+        return
+
+    users = response_users.json()
+    todos = response_todos.json()
+
+    user_info = next((user for user in users if user['id'] == id_employee), None)
+    if not user_info:
+        print("Employee with ID {} not found.".format(id_employee))
+        return
+
+    completed_tasks = [todo['title'] for todo in todos if todo['userId'] == id_employee and todo['completed']]
+    total_tasks = len(completed_tasks) + user_info['id']
+
+    print('Employee {} is done with tasks({}/{}):'.format(user_info['name'], len(completed_tasks), total_tasks))
+    for title in completed_tasks:
+        print('\t{}'.format(title))
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-    else:
-        employee_id = int(sys.argv[1])
-        fetch_employee_todo_progress(employee_id)
+    information_employee()
