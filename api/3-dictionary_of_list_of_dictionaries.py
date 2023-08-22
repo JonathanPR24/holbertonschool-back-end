17e62ab540fb843924fc01d6de0281efb2aaea36
JonathanPR24 <jonathanperez604@gmail.com>
2023-08-22 16:40:15 -0400

    Dictionary of list of dictionaries

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,28 +1,40 @@
 #!/usr/bin/python3
 """Script to return info about todo list progress"""
-import requests
-import sys
 import json
+import requests
 
-if __name__ == "__main__":
-    employee_id = int(sys.argv[1])
-
-    api_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+def get_user_tasks(user_id):
+    api_url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
     response = requests.get(api_url)
-    employee_name = response.json()["name"]
+    user_data = response.json()
 
-    api_url2 = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
+    api_url2 = f'https://jsonplaceholder.typicode.com/todos?userId={user_id}'
     response = requests.get(api_url2)
     tasks = response.json()
-    
-    completed_tasks = []
+
+    user_tasks = []
     for task in tasks:
-        completed_tasks.append({
-            "username": employee_name,
+        user_tasks.append({
+            "username": user_data["username"],
             "task": task["title"],
             "completed": task["completed"]
         })
 
-    with open("todo_all_employees.json", "a") as json_file:
-        json.dump({employee_id: completed_tasks}, json_file)
-        json_file.write("\n")
+    return user_tasks
+
+def main():
+    api_users_url = 'https://jsonplaceholder.typicode.com/users'
+    response = requests.get(api_users_url)
+    users = response.json()
+
+    json_dict = {}
+
+    for user in users:
+        user_id = user["id"]
+        json_dict[user_id] = get_user_tasks(user_id)
+
+    with open("todo_all_employees.json", 'w') as json_file:
+        json.dump(json_dict, json_file, indent=4)
+
+if __name__ == "__main__":
+    main()
